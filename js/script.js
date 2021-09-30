257d4f9b006b2892d37f57d68eb8931517ed903c
Mark <74104639+MarkvHeusden@users.noreply.github.com>
2021-09-30 14:22:38 +0200

    JS Cleanup

--- js/script.js
+++ js/script.js
@@ -14,17 +14,9 @@ async function init() {
     const modelURL = URL + "model.json";
     const metadataURL = URL + "metadata.json";
 
-
-
-    // load the model and metadata
-    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-    // Note: the pose library adds a tmPose object to your window (window.tmPose)
     model = await tmPose.load(modelURL, metadataURL);
     maxPredictions = model.getTotalClasses();
 
-
-
-    // Convenience function to setup a webcam
     const size = 200;
     const flip = true; // whether to flip the webcam
     webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
@@ -32,9 +24,6 @@ async function init() {
     await webcam.play();
     window.requestAnimationFrame(loop);
 
-
-
-    // append/get elements to the DOM
     const canvas = document.getElementById("canvas");
     canvas.width = size;
     canvas.height = size;
@@ -44,13 +33,14 @@ async function init() {
         labelContainer.appendChild(document.createElement("li"));
     }
 
+    // Verwijder start button als applicatie is geladen
     startButton.remove();
 }
 
 
 
 async function loop(timestamp) {
-    webcam.update(); // update the webcam frame
+    webcam.update();
     await predict();
     window.requestAnimationFrame(loop);
 }
@@ -58,29 +48,24 @@ async function loop(timestamp) {
 
 
 async function predict() {
-    // Prediction #1: run input through posenet
-    // estimatePose can take in an image, video or canvas html element
     const {
         pose,
         posenetOutput
     } = await model.estimatePose(webcam.canvas);
-    // Prediction 2: run input through teachable machine classification model
     const prediction = await model.predict(posenetOutput);
 
 
 
     for (let i = 0; i < maxPredictions; i++) {
         const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+            prediction[i].className + ": " + prediction[i].probability.toFixed(1);
         labelContainer.childNodes[i].innerHTML = classPrediction;
 
-        console.log("test");
+        // Als je niet voor 95% goed zit krijg je een rood bolletje
         if (prediction[0].probability.toFixed(2) >= 0.95) {
-            statusElement.classList.remove('bad');
-            statusElement.classList.add('good');
+            statusElement.className = 'good';
         } else {
-            statusElement.classList.add('bad');
-            statusElement.classList.remove('good');
+            statusElement.className = 'bad';
         }
     }
 
@@ -95,12 +80,8 @@ async function predict() {
 function drawPose(pose) {
     if (webcam.canvas) {
         ctx.drawImage(webcam.canvas, 0, 0);
-        // draw the keypoints and skeleton
         if (pose) {
             const minPartConfidence = 0.5;
-            // const strokeColor = 'red';
-            // tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx, strokeColor);
-            // tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx, strokeColor);
             tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
             tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
         }
